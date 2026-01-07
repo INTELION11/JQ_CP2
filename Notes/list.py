da434159fbccef7d051bb0f996118c6cee0e79fb
INTELION11 <jacob.quezada@ucas-edu.net>
2026-01-07 09:58:34 -0700

    I FORGOT

--- Notes/list.py
+++ Notes/list.py
@@ -1,3 +1,16 @@
 # Vl 2nd Types of Lists Notes
 #this is new 
-print("hellow wrld")
+import random as r
+print("hellow world")
+num = 1000
+LIGHT_GREEN = "\033[92m"
+RESET = "\033[0m" # Code to reset the text color to default
+
+while True:
+    
+    two = r.random()
+    three = two * 10000
+    yes = num * three
+    print(f"{LIGHT_GREEN}  {int(yes)}")
+
+    num = 10000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
