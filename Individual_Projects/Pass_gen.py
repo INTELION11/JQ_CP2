e9789b1bc1b217ed59e28e35bf6c600db766e384
INTELION11 <jacob.quezada@ucas-edu.net>
2026-01-27 10:49:08 -0700

    almost done

--- Individual_Projects/Pass_gen.py
+++ Individual_Projects/Pass_gen.py
@@ -8,34 +8,38 @@ def pass_gen():
                 return password
             case "n":
                 return
-    len = input("How long does the password need to be:\n").strip().lower()
+    leng = int(input("How long does the password need to be:\n"))
     lower = input("Does the password need lowercase letters (Y/N):\n").strip().lower()
     upper = input("Does the password need uppercase letters (Y/N):\n").strip().lower()
     num = input("Does the password need numbers letters (Y/N):").strip().lower()
     char = input("Does the password need special characters letters (Y/N):\n").strip().lower()
     password = []
-    for i in range(1,3):
-        for i in range(1,len):
-            pas_add(upper,password,97,122); continue 
-            pas_add(lower,password,65,90); continue
-            pas_add(num,password,0,127); continue
-            pas_add(char,password,97,122); continue
+    for i in range(1,4):
+        for i in range(1,leng):
+            pas_add(upper,password,97,122); exit if len(password) == leng else print()
+
+            pas_add(lower,password,65,90); exit if len(password) == leng else print()
+
+            pas_add(num,password,0,127); exit if len(password) == leng else print()
+
+            pas_add(char,password,97,122); exit if len(password) == leng else print()
+
         new_password = []
         for i in password:
-            new_password.append(ord(i))
-        print(new_password)
+            new_password.append(chr(i))
+        print(*new_password,sep="")
 
         #underscore if its under none of the above
 
 
-
-while True:
+world = True
+while world == True:
     action = input("\n1. Generate Passwords \n2. Exit\n").strip().lower()
     match action:
         case "1":
             pass_gen()
         case "2":
-            exit
+            world = False
         case _:
             print("please try again")
     
