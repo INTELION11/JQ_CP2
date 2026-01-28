1f66d59dda1d5d60c9143500a19b855b7a61086c
INTELION11 <jacob.quezada@ucas-edu.net>
2026-01-28 02:22:41 +0000

    finito

--- Individual_Projects/Pass_gen.py
+++ Individual_Projects/Pass_gen.py
@@ -1,45 +1,39 @@
-# JQ 2nd Password Generator
-import random as r
-def pass_gen():
-    def pas_add(case,password,num1,num2):
-        match case:
-            case "y":
-                password.append(r.randint(num1,num2))
-                return password
-            case "n":
-                return
-    leng = int(input("How long does the password need to be:\n"))
-    lower = input("Does the password need lowercase letters (Y/N):\n").strip().lower()
-    upper = input("Does the password need uppercase letters (Y/N):\n").strip().lower()
-    num = input("Does the password need numbers letters (Y/N):").strip().lower()
-    char = input("Does the password need special characters letters (Y/N):\n").strip().lower()
-    password = []
+# JQ 2nd Password Generator  
+import random as r  
+  
+def pass_gen():  
+    def pas_add(case, password, num1, num2):  
+        if case == "y":  
+            password.append(r.randint(num1, num2))  
+  
+    leng = int(input("How long does the password need to be:\n"))  
+    lower = input("Does the password need lowercase letters (Y/N):\n").strip().lower()  
+    upper = input("Does the password need uppercase letters (Y/N):\n").strip().lower()  
+    num = input("Does the password need numbers (Y/N):\n").strip().lower()  
+    char = input("Does the password need special characters (Y/N):\n").strip().lower()  
     for i in range(1,4):
-        for i in range(1,leng):
-            pas_add(upper,password,97,122); exit if len(password) == leng else print()
-
-            pas_add(lower,password,65,90); exit if len(password) == leng else print()
-
-            pas_add(num,password,0,127); exit if len(password) == leng else print()
-
-            pas_add(char,password,97,122); exit if len(password) == leng else print()
-
-        new_password = []
-        for i in password:
-            new_password.append(chr(i))
-        print(*new_password,sep="")
-
-        #underscore if its under none of the above
-
-
-world = True
-while world == True:
-    action = input("\n1. Generate Passwords \n2. Exit\n").strip().lower()
-    match action:
-        case "1":
-            pass_gen()
-        case "2":
-            world = False
-        case _:
-            print("please try again")
+        password = []  
+        while len(password) <= leng:  
+            pas_add(lower, password, 97, 122)    
+            if len(password) == leng: break  
+            pas_add(upper, password, 65, 90)      
+            if len(password) == leng: break  
+            pas_add(num, password, 48, 57)       
+            if len(password) == leng: break  
+            if char == "y":  
+                spec = list('!@#\$%^&*()_+-=[]{}|;:,.&lt;&gt;?/~`')  
+                password.append(ord(r.choice(spec)))  
+            if len(password) == leng: break  
     
+        print("".join([chr(i) for i in password]))  
+    
+world = True  
+while world:  
+    action = input("\n1. Generate Passwords \n2. Exit\n").strip().lower()  
+    match action:  
+        case "1":  
+            pass_gen()  
+        case "2":  
+            world = False  
+        case _:  
+            print("please try again")  
