0328ee677975c3e69ceed0a0a9c42d451e68e674
INTELION11 <jacob.quezada@ucas-edu.net>
2026-01-29 17:51:30 +0000

    yo

---
+++ Notes/test.py
@@ -0,0 +1,10 @@
+#thing
+import sys
+import time as t
+def sprint(text, delay=0.015):  
+    for char in text:  
+        sys.stdout.write(char)  
+        sys.stdout.flush()  
+        t.sleep(delay)  
+    print() 
+sprint("good job mymna ldikxjgesjhbhzshiuywshsvishzbiuhsysbpjhbujhzskugvzs")
