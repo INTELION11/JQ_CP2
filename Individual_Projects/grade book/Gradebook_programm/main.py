0e6707e14a7d90e484001ed3cfbac80e361b2265
INTELION11 <jacob.quezada@ucas-edu.net>
2026-03-27 10:49:09 -0600

    dono

--- Individual_Projects/grade book/Gradebook_programm/main.py
+++ Individual_Projects/grade book/Gradebook_programm/main.py
@@ -1,9 +1,10 @@
 def studenter():
 
     class student:
-        def __init__(self,name,grade_log = []):
+        def __init__(self,name,id,grade_log = []):
             self.name = name
             self.grade_log = grade_log
+            self.id = id
  
         def avarge_grade(self,grade):
             self.grade_log.append(grade)
@@ -11,6 +12,7 @@ def studenter():
         def view_catalog(self):
             for book in self.grade_log:
                 print(book)
+            print(self.name)
 
 
     class grade:
@@ -21,21 +23,25 @@ def studenter():
         def __str__(self):
             return f"{self.classs}: {self.grade}"
         
-    lib = student("Provo Library")
+    lib = student("jack of arc",1890)
 
 
-    lib.avarge_grade(grade("The way of kings","89"))
-
-    lib.view_catalog()
+    lib.avarge_grade(grade("mathclass","89"))
 
 
-
-
-
-option = input("\n🎯 MAIN MENU:\n[1] Add New Student\n[2] Add Grade to Student\n[3] View Student Record\n[4] View All Students\n[5] Class Summary\n[6] Exit\n")
-if option == "1":
-    studenter()
-if option == "2":
+    universe = True
+    while universe == True:
+        option = input("\n🎯 MAIN MENU:\n[1] Add New Student\n[2] Add Grade to Student\n[3] View Student Record\n[4] View All Students\n[5] Class Summary\n[6] Exit\n")
+        if option == "1":
+            opt = input("What ")
+            studenter()
+        elif option == "2":
+            add_grade()
+        elif option == "5":
+            lib.view_catalog()
+        elif option == "6":
+            universe = False
+        
+        
+studenter()
     
-
-
